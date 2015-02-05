bfee8526698127d5aef4a2c912c59dfa2625ea52
unknown <telus-sgupta@premium-cloud-support.com>
2015-02-05 13:48:56 -0500

    testingGO1

--- hello_test.go
+++ hello_test.go
@@ -1,20 +1,20 @@
-package hello
+package newsletter
 
 import (
-    "fmt"
-    "net/http"
-    "testing"
+        "reflect"
+        "testing"
+
+        "appengine/mail"
 )
 
-func init() {
-    http.HandleFunc("/", handler)
+func TestComposeNewsletter(t *testing.T) {
+        want := &mail.Message{
+                Sender:  "newsletter@appspot.com",
+                To:      []string{"User <user@example.com>"},
+                Subject: "Weekly App Engine Update",
+                Body:    "Don't forget to test your code!",
+        }
+        if msg := composeNewsletter(); !reflect.DeepEqual(msg, want) {
+                t.Errorf("composeMessage() = %+v, want %+v", msg, want)
+        }
 }
-
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world12!")
-    testmodle();
-}
-
-func testmodle(t *testing.T) {
-        fmt.Fprint("TestSuccess")
-	}
